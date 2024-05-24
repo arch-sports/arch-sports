0b954edea1c1ac77693398bac793c5403934df70
arch-sports <tanner.clark@architechsports.com>
2024-05-23 23:04:36 -0400

    Reporting

---
+++ Basketball Reporting/individual_report_script.R
@@ -0,0 +1,24 @@
+library(quarto)
+library(knitr)
+library(fs)
+
+time <- Sys.time()
+quarto_render('basketball_report.qmd',
+              output_format = 'pdf',
+              output_file = 'Pro Skills Basketball Test Results.pdf')
+
+file_move('Pro Skills Basketball Test Results.pdf', './Reports/Pro Skills Basketball Test Results.pdf')
+
+data <- read.csv(file = './Data/basketball_random.csv')
+athletes <- data$Athlete
+
+for (athlete in athletes) {
+  quarto_render('Individual_Report.qmd',
+              output_format = 'pdf',
+              output_file = paste(athlete, '.pdf', sep = ""),
+              execute_params = list(Athlete = athlete))
+
+  fs::file_move(paste(athlete, '.pdf', sep = ""), paste('./Reports/Individual Reports/', athlete, '.pdf', sep = ""))
+}
+
+print(paste('Time taken:', Sys.time() - time))
