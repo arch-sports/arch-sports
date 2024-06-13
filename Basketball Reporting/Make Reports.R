f371020322d55ea802dfbc0566876584adbe73a8
arch-sports <tanner.clark@architechsports.com>
2024-06-13 18:00:22 -0400

    new reports

--- Basketball Reporting/Make Reports.R
+++ Basketball Reporting/Make Reports.R
@@ -17,7 +17,8 @@ time <- Sys.time()
 
 
 dir.create('./Reports/', showWarnings = FALSE)
-dir.create('./Reports/Individual Reports', showWarnings = FALSE)
+dir.create('./Reports/Individual Reports - Athletes', showWarnings = FALSE)
+dir.create('./Reports/Individual Reports - Technical', showWarnings = FALSE)
 
 athletes <- data$Athlete[!is.na(data$jump_height)]
 
@@ -30,8 +31,17 @@ for (athlete in athletes) {
               output_format = 'pdf',
               output_file = paste(athlete, '.pdf', sep = ""),
               execute_params = list(Athlete = athlete))
+  
 
-  file_move(paste(athlete, '.pdf', sep = ""), paste('./Reports/Individual Reports/', athlete, '.pdf', sep = ""))
+  file_move(paste(athlete, '.pdf', sep = ""), paste('./Reports/Individual Reports - Athletes/', athlete, '.pdf', sep = ""))
+  
+  quarto_render('Individual_Report_Technical.qmd',
+                output_format = 'pdf',
+                output_file = paste(athlete, '- Technical.pdf', sep = ""),
+                execute_params = list(Athlete = athlete))
+  
+  file_move(paste(athlete, '.pdf', sep = ""), paste('./Reports/Individual Reports - Technical/', athlete, '.pdf', sep = ""))
+  
 }
 
 
